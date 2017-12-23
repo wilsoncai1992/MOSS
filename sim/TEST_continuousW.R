96d873cec3f31930253f64bc16177374555536db
Wilson Cai <wcai@berkeley.edu>
2017-12-22 19:38:59 -0800

    continuous W

--- sim/TEST_continuousW.R
+++ sim/TEST_continuousW.R
@@ -5,15 +5,16 @@ library(simcausal)
 D <- DAG.empty()
 
 D <- D +
-  node("W", distr = "runif", min = 0, max = 2) +
-  node("A", distr = "rbinom", size = 1, prob = .3 + .3*W) +
+  # node("W", distr = "runif", min = -1, max = 0) +
+  node("W", distr = "runif", min = 0, max = 1) +
+  node("A", distr = "rbinom", size = 1, prob = .2 + .5*W) +
   # node("A", distr = "rbinom", size = 1, prob = .5) +
   node("Trexp", distr = "rexp", rate = 1 + .5*W - .5*A) +
   # node("Trexp", distr = "rexp", rate = 1 + 2*W - .5*A) +
   node("Cweib", distr = "rweibull", shape = .7 - .2*W, scale = 1) +
   node("T", distr = "rconst", const = round(Trexp*10,0)) +
-  # node("C", distr = "rconst", const = round(Cweib*10, 0)) +
-  node("C", distr = "rconst", const = 999) +
+  node("C", distr = "rconst", const = round(Cweib*10, 0)) +
+  # node("C", distr = "rconst", const = 999) +
   node("T.tilde", distr = "rconst", const = ifelse(T <= C , T, C)) +
   node("Delta", distr = "rconst", const = ifelse(T <= C , 1, 0))
 setD <- set.DAG(D)
@@ -44,7 +45,7 @@ true_func <- function(x,A,W){
   1 - pexp(x/10, rate = 1+.5*W-.5*A)
 }
 plot_one_arm <- function(A, ...) {
-  meshgrid <- expand.grid(seq(0,30,.01), A, seq(0,2,.1))
+  meshgrid <- expand.grid(seq(0,50,.1), A, seq(0,1,.1))
   p_hat <- apply(meshgrid, 1, function(x) true_func(x[1],x[2],x[3]))
   df_temp <- data.frame(meshgrid, p_hat)
   colnames(df_temp)[1] <- 'x'
@@ -55,12 +56,9 @@ plot_one_arm(A = 1, col = 'red')
 plot_one_arm(A = 0, col = 'blue')
 
 # R6
-# onestepfit = MOSS$new(dat, dW = 1, verbose = TRUE, epsilon.step = 1e-3, max.iter = 1e2)
 onestepfit = MOSS$new(dat, dW = 1,
   # verbose = TRUE, epsilon.step = 1e-3, max.iter = 5e2)
-  verbose = TRUE, epsilon.step = 1e-1, max.iter = 5e2)
-  # verbose = TRUE, epsilon.step = 5e-4, max.iter = 5e2)
-  # verbose = TRUE, epsilon.step = 1e-4, max.iter = 5e2)
+  verbose = TRUE, epsilon.step = 1e-2, max.iter = 5e2)
 # onestepfit$initial_fit(g.SL.Lib = c("SL.glm", "SL.step", "SL.glm.interaction"),
 onestepfit$initial_fit(g.SL.Lib = c("SL.mean","SL.glm", 'SL.gam'),
                        Delta.SL.Lib = c("SL.mean","SL.glm", "SL.gam"),
@@ -79,6 +77,7 @@ stopping <- onestepfit$compute_stopping()
 while ((stopping >= onestepfit$tol) & (iter_count <= onestepfit$max.iter)) {
 # while ((stopping >= onestepfit$tol) & (iter_count <= onestepfit$max.iter) & ((stopping_prev - stopping) >= max(-onestepfit$tol, -1e-5))) {
   print(stopping)
+  if (stopping_prev < stopping) onestepfit$epsilon.step <- -onestepfit$epsilon.step
   # onestepfit$onestep_curve_update()
   # onestepfit$onestep_curve_update_pooled()
   onestepfit$onestep_curve_update_mat()
