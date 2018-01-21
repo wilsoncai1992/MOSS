6ff7dafae811426103c4a50c9b57ebda38a076c2
Wilson Cai <wcai@berkeley.edu>
2018-01-21 11:01:16 -0800

    complete diff curve taget

--- R/onestepSurvivalDifferenceR6.R
+++ R/onestepSurvivalDifferenceR6.R
@@ -4,33 +4,48 @@ require("SuperLearner")
 #' @export
 MOSS_difference <- R6Class("MOSS_difference",
   public = list(
+    dat = NULL,
+    epsilon.step = NULL,
+    max.iter = NULL,
+    tol = NULL,
+    T.cutoff = NULL,
+    verbose = NULL,
+# single MOSS object
+    MOSS_A1 = NULL,
+    MOSS_A0 = NULL,
+# targeting
+    D1.t = NULL,
+    Pn.D1.t = NULL,
+    stopping_history = numeric(),
+    Psi.hat = NULL,
     # simultaneous CI
     simul_CI = NULL,
     initialize = function(dat,
-                          dW,
                           epsilon.step = 1e-5,
                           max.iter = 1e3,
                           tol = 1/nrow(dat),
                           T.cutoff = NULL,
                           verbose = FALSE) {
-      self$dat <- dat
-      self$dW <- dW
-      self$epsilon.step <- epsilon.step
-      self$max.iter <- max.iter
-      self$tol <- tol
-      self$T.cutoff <- T.cutoff
-      self$verbose <- verbose
 
-      self$check_and_preprocess_data(T.cutoff = self$T.cutoff)
-      self$update_tensor <- matrix(0, nrow = self$n_sample, ncol = length(self$T.uniq))
-      # self$inside_exp <- rep(0, length(self$T.uniq))
-      self$inside_exp <- matrix(0, ncol = length(self$T.uniq), nrow = self$n_sample)
+      self$MOSS_A1 <- MOSS$new(dat, dW = 1,
+                              epsilon.step = epsilon.step,
+                              max.iter = max.iter,
+                              tol = tol,
+                              T.cutoff = T.cutoff,
+                              verbose = verbose)
+      self$MOSS_A0 <- MOSS$new(dat, dW = 0,
+                              epsilon.step = epsilon.step,
+                              max.iter = max.iter,
+                              tol = tol,
+                              T.cutoff = T.cutoff,
+                              verbose = verbose)
 
-      self$MOSS_A1 <- onestepfit = MOSS$new(dat, dW = 1,
-                verbose = self$verbose, epsilon.step = epsilon.step, max.iter = max.iter)
-      self$MOSS_A0 <- onestepfit = MOSS$new(dat, dW = 0,
-                verbose = self$verbose, epsilon.step = epsilon.step, max.iter = max.iter)
-
+      self$dat <- self$MOSS_A1$dat
+      self$epsilon.step <- self$MOSS_A1$epsilon.step
+      self$max.iter <- self$MOSS_A1$max.iter
+      self$tol <- self$MOSS_A1$tol
+      self$T.cutoff <- self$MOSS_A1$T.cutoff
+      self$verbose <- self$MOSS_A1$verbose
     },
     initial_fit = function(g.SL.Lib = c("SL.mean","SL.glm", 'SL.gam'),
                            Delta.SL.Lib = c("SL.mean","SL.glm", "SL.gam"),
@@ -47,6 +62,58 @@ MOSS_difference <- R6Class("MOSS_difference",
       self$MOSS_A0$transform_failure_hazard_to_survival()
       self$MOSS_A0$transform_failure_hazard_to_pdf()
       self$MOSS_A0$compute_EIC()
-    }
+    },
+    onestep_diff_curve = function(){
+      browser()
+      self$D1.t <- self$MOSS_A1$D1.t - self$MOSS_A0$D1.t
+      self$Pn.D1.t <- colMeans(self$D1.t)
+      self$MOSS_A1$D1.t <- self$D1.t
+      self$MOSS_A0$D1.t <- self$D1.t
+      self$MOSS_A1$Pn.D1.t <- self$Pn.D1.t
+      self$MOSS_A0$Pn.D1.t <- self$Pn.D1.t
+
+      iter_count <- 0
+      stopping_prev <- Inf
+
+      stopping <- self$MOSS_A1$compute_stopping()
+      while ((stopping >= self$tol) & (iter_count <= self$max.iter)) {
+      # while ((stopping >= self$tol) & (iter_count <= self$max.iter) & ((stopping_prev - stopping) >= max(-self$tol, -1e-5))) {
+        print(stopping)
+        if (stopping_prev < stopping) onestepfit$epsilon.step <- -onestepfit$epsilon.step
+        # self$onestep_curve_update()
+        self$MOSS_A1$onestep_curve_update_mat()
+        self$MOSS_A0$onestep_curve_update_mat()
+        self$MOSS_A1$compute_EIC()
+        self$MOSS_A0$compute_EIC()
+
+        self$D1.t <- self$MOSS_A1$D1.t - self$MOSS_A0$D1.t
+        self$Pn.D1.t <- colMeans(self$D1.t)
+        self$MOSS_A1$D1.t <- self$D1.t
+        self$MOSS_A0$D1.t <- self$D1.t
+        self$MOSS_A1$Pn.D1.t <- self$Pn.D1.t
+        self$MOSS_A0$Pn.D1.t <- self$Pn.D1.t
+
+        iter_count <- iter_count + 1
+        self$stopping_history[iter_count] <- stopping
+        stopping_prev <- self$stopping_history[iter_count]
+        stopping <- self$MOSS_A1$compute_stopping()
+
+        # if (iter_count %% 10 == 0) onestepfit$plot_onestep_curve(add = TRUE)
+        # if (iter_count %% 10 == 0) plot(onestepfit$Pn.D1.t); abline(h = 0)
+      }
+
+      if (iter_count == self$max.iter) {
+        warning('Max Iter count reached, stop iteration.')
+      }
+
+      self$MOSS_A1$compute_Psi()
+      self$MOSS_A0$compute_Psi()
+      self$Psi.hat <- self$MOSS_A1$Psi.hat - self$MOSS_A0$Psi.hat
+    },
+    print = function(){
+      out <- data.frame(self$T.uniq, self$Psi.hat, self$sd_EIC, self$upper_CI, self$lower_CI)
+      colnames(out) <- c('Time', 'survival curve', 'std_err', 'upper_CI', 'lower_CI')
+      return(out)
+    },
   )
 )
