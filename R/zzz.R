e73eab3bd6980a1a6c38f540db51486e37e77647
Wilson Cai <wcai@berkeley.edu>
2019-04-16 10:38:40 -0700

    add cran check

--- R/zzz.R
+++ R/zzz.R
@@ -1,6 +1,6 @@
 .onAttach <- function(...) {
   packageStartupMessage(paste0(
     "MOSS v", utils::packageDescription("MOSS")$Version,
-    ": model one-step survival"
+    ": Model One-Step Survival"
   ))
 }
